1340ac752b9d23ceb44c00675157ad644b88c495
baxiyi <mzw5810@gmail.com>
2019-06-23 21:23:49 +0800

    Updates

---
+++ precache-manifest.8c52c47079736293b4e750168530a8cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b1b887b26d5d78a3b5aff7d32437bb",
+    "url": "/hello-React/index.html"
+  },
+  {
+    "revision": "b4dd35090b0841278df2",
+    "url": "/hello-React/static/css/main.f4b9b883.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/hello-React/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "b4dd35090b0841278df2",
+    "url": "/hello-React/static/js/main.115772ea.chunk.js"
+  },
+  {
+    "revision": "cf82cba4e4f265202649",
+    "url": "/hello-React/static/js/runtime~main.d049004f.js"
+  }
+]);
